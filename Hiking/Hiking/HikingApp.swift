8267336bc7739894e6223fec4a08cab0ee6a749a
Avinash Kumar <avinash@Avinashs-MacBook-Pro.local>
2025-03-14 00:54:55 +0530

    Hiking

---
+++ Hiking/Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Avinash Kumar on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
