12e0afedabed1e7a3150206a0e7d66344db3653b
Avinash Kumar <avinash@Avinashs-MacBook-Pro.local>
2025-03-13 20:22:21 +0530

    Learning SwiftUI

---
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by Avinash Kumar on 13/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Avinash Kumar on 13/03/25.
+//
+
+import SwiftUI
+
+@main // - starting point of application
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // view to render
+        }
+    }
+}
