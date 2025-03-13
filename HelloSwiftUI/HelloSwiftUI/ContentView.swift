3448ac592f0d23a418dfdc8c43c847b7041655a4
Avinash Kumar <avinash@Avinashs-MacBook-Pro.local>
2025-03-13 20:45:54 +0530

    VStackHStackandText

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 5) {
+            Text("Hello World")
+                .foregroundStyle(.cyan)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Text("Hi Avinash")
+                .foregroundStyle(.green)
+            Text("Hello SwiftUI")
+            HStack(spacing: 20) {
+                Text("Hello World")
+                Text("Hi Avinash")
+                Text("Hello SwiftUI")
+            }.foregroundColor(.red)
+        }.foregroundColor(.orange)
     }
 }
 
