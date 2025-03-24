9da29592589e7bceb10339a421a3c941d6897f24
Avinash Kumar <avinash@Avinashs-MacBook-Pro.local>
2025-03-24 15:37:09 +0530

    Displaying Hikes in a swift UI List

--- Hiking/Hiking/ContentView.swift
+++ Hiking/Hiking/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let hikes = [
+        Hike(name: "Sal", photo: "sal", miles: 6),
+        Hike(name: "Tam", photo: "tam", miles: 5.8),
+        Hike(name: "Tom", photo: "tom", miles: 10)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(hikes) { hike in
+            HStack(alignment: .top) {
+                Image(hike.photo)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    .frame(width: 100)
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                    Text("Distance: \(hike.miles.formatted()) miles")
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ Hiking/Hiking/Models/Hike.swift
@@ -0,0 +1,15 @@
+//
+//  Hike.swift
+//  Hiking
+//
+//  Created by Avinash Kumar on 24/03/25.
+//
+
+import Foundation
+
+struct Hike: Identifiable {
+    let id = UUID()
+    let name: String
+    let photo: String
+    let miles: Double
+}
